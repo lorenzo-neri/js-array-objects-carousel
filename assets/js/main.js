e0c36d3f83765ded76d5ce5f65a96b062e9f08a8
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-07 14:41:41 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* 
+Riprendiamo il live coding visto in classe un pó di tempo fá sul carosello di immagini e rifacciamolo, questa volta usando gli oggetti.
+
+Potete prendere come riferimento il codice scritto insieme nel live, che troverete direttamente nella mia repository di github a questo link: [https://github.com/fabiopacifici/104_js/tree/main/live_slider]
+
+Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto di un array di stringhe.
+
+Bonus 0:
+Non eramamo ancora a conoscenda di molti strumenti utili, come ad esempio le funzioni. É possibile fare refactoring del codice, pulendolo e creando quanche funzione che possa rendere tutto piú leggibile e pulito?
+
+Bonus 1:
+Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
+al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
+
+Bonus 2:
+E se volessi un bottone per invertire la "direzione" del carosello?
+*/
